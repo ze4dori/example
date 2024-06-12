9e259d797f17eb635c9f106b91e8bb4642ddcd77
ze4dori <162151156+ze4dori@users.noreply.github.com>
2024-06-12 12:09:03 +0300

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from config import host, user, password, db_name
-
-connection = psycopg2.connect(
-        host=host, 
-        user=user, 
-        password=password,
-        database=db_name
-     )
